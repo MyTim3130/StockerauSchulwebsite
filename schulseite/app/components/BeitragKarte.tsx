dd4d90c2e7b7ea697bf69be284fd306e5897f2e3
MyTim3130 <tin.hauslkramper@gmail.com>
2024-01-31 19:09:53 +0100

    forLaptop

--- schulseite/app/components/BeitragKarte.tsx
+++ schulseite/app/components/BeitragKarte.tsx
@@ -4,18 +4,35 @@ import axios from 'axios';
 import { STRAPI_URL } from '@/globaleVariablen';
 import { BeitragsData } from './Hero';
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+import { useState, useEffect } from 'react';
 
 
 
-async function BeitragKarte(){
+export default function BeitragKarte(){
 
-    const response3 = await axios.get(
-        "http://localhost:1337/api/beitraege?populate=*"
-      );
-      /*  console.log(response3.data.data);*/
+const [response3, setResponse3] = useState<any>({})
+
+useEffect(()=>{
+  const load = async () => {
+    const res = await axios.get(
+      "http://localhost:1337/api/beitraege?populate=*"
+    );
+    /*  console.log(response3.data.data);*/
+
+    setResponse3(res)
+  }
+  load();
+},[])
+
+
+if (Object.keys(response3).length == 0){
+  return null
+}
+
+
   return (
   
-    <div className="sm:grid grid-cols-2 w-[80vw]">
+    <div className="sm:grid grid-cols-2 w-[80vw] paddingFix">
     {response3.data.data
       .slice(-4)
       .map((data: BeitragsData, index: number) => {
@@ -26,7 +43,7 @@ async function BeitragKarte(){
           <CardBody className="flex flex-col items-center mt-12 sm:mb-10">
             <CardItem className="flex items-center flex-col w-fit">
               <img
-                className="sm:h-52 lg:h-72 xl:h-96 rounded-md h-52"
+                className="sm:h-52 lg:h-72 xl:h-96 rounded-md h-52 object-cover"
                 src={
                   STRAPI_URL + data.attributes.Cover.data.attributes.url
                 }
@@ -47,5 +64,3 @@ async function BeitragKarte(){
 
   )
 }
-
-export default BeitragKarte
